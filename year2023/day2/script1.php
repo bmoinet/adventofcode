48592da1e0482c97e629568eb1a179601efc810c
Bastien Moinet <bmoinet@upenn.edu>
2023-12-02 11:45:27 +0100

    Year 2023, day 2

---
+++ year2023/day2/script1.php
@@ -0,0 +1,40 @@
+<?php
+
+$file = file_get_contents('./input.txt');
+$file = explode("\n", $file);
+
+$sum = 0;
+
+foreach ($file as $line) {
+
+    preg_match_all('/Game (\d+):/', $line, $matches);
+    list($items, $number) = $matches;
+    $number = (int)$number[0];
+    //var_dump('number', $number);
+
+    preg_match_all('/(\d+) (red|blue|green)/', $line, $matches);
+    list($items, $values, $colors) = $matches;
+    //var_dump($number, $values, $colors); die;
+
+    $status = ['red' => true, 'green' => true, 'blue' => true];
+
+    $maxis = ['red' => 12, 'green' => 13, 'blue' => 14];
+
+    $n = -1;
+    foreach ($colors as $color) {
+        $value = $values[++$n];
+        $status[$color] = $status[$color] && ($value <= $maxis[$color]);
+    }
+
+    //var_dump('status', $status);
+    //if ($number === 4) die;
+
+    $ok = ($status['red'] && $status['green'] && $status['blue']);
+
+    if ($ok) {
+        //echo "$number !\n";
+        $sum += $ok ? $number : 0;
+    }
+}
+
+echo $sum;
---
+++ year2023/day2/script2.php
@@ -0,0 +1,36 @@
+<?php
+
+$file = file_get_contents('./input.txt');
+$file = explode("\n", $file);
+
+$sum = 0;
+
+foreach ($file as $line) {
+
+    preg_match_all('/Game (\d+):/', $line, $matches);
+    list($items, $number) = $matches;
+    $number = (int)$number[0];
+    //var_dump('number', $number);
+
+    preg_match_all('/(\d+) (red|blue|green)/', $line, $matches);
+    list($items, $values, $colors) = $matches;
+    //var_dump($number, $values, $colors); //die;
+
+    $maxis = ['red' => 0, 'green' => 0, 'blue' => 0];
+
+    $n = -1;
+    foreach ($colors as $color) {
+        $value = $values[++$n];
+        $maxis[$color] = ($value > $maxis[$color]) ? $value : $maxis[$color];
+    }
+
+    //var_dump('$maxis', $maxis);
+    //if ($number === 1) die;
+
+    $power = ($maxis['red'] * $maxis['green'] * $maxis['blue']);
+    //var_dump('$power', $power);
+
+    $sum += $power;
+}
+
+echo $sum;
